1def4673d0a7f7665699d381e0cb7ef2f8156afe
Anurag Chaurasiya <anurag91920@gmail.com>
2025-09-09 16:08:27 +0530

    Second Commit

--- frontend/src/Landing_page/Footer.js
+++ frontend/src/Landing_page/Footer.js
@@ -1,69 +1,74 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import "./Footer.css";
 
 function Footer() {
     return (
-    <footer style={{backgroundColor: "rgb(250, 250, 250)"}}> 
-        <div className='container border-top mt-5'>
+    <footer style={{backgroundColor: "#080404"}} className="p-4"> 
+        <div className='container con border-top mt-5'>
             <div className='row mt-5'>
                 <div className='col'>
                     <img src='media/images/Logo.webp' style={{ width: "40%" }} alt='Logo' />
                     <p>
-                        &copy; © 2010 - 2025, TradeNest Broking Ltd. All rights reserved.
+                        &copy; 2010 - 2025, TradeNest Broking Ltd. All rights reserved.
                     </p>
                     <div className='mt-2 fs-2'>
-                    <i class="fa-brands fa-facebook p-2"></i>
-                    <i class="fa-brands fa-linkedin p-2"></i>
-                    <i class="fa-brands fa-github p-2"></i>
-                    <i class="fa-brands fa-x-twitter p-2"></i>
-                    <br />
-                    <hr />
-                    <i class="fa-brands fa-instagram p-2"></i>
-                    <i class="fa-brands fa-youtube p-2"></i>
-                    <i class="fa-brands fa-whatsapp p-2"></i>
+                            {/* Social Media Links */}
+                            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                                <i className="fa-brands fa-facebook p-2"></i>
+                            </a>
+                            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                                <i className="fa-brands fa-linkedin p-2"></i>
+                            </a>
+                            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                                <i className="fa-brands fa-github p-2"></i>
+                            </a>
+                            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                                <i className="fa-brands fa-x-twitter p-2"></i>
+                            </a>
+                            <hr />
+                            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                                <i className="fa-brands fa-instagram p-2"></i>
+                            </a>
+                            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+                                <i className="fa-brands fa-youtube p-2"></i>
+                            </a>
+                            <a href="https://wa.me/yourNumber" target="_blank" rel="noopener noreferrer">
+                                <i className="fa-brands fa-whatsapp p-2"></i>
+                            </a>
+                        </div>
+                </div>
+                {/* Company Section */}
+                    <div className='col-6 col-md-3 mb-4 footer-links'>
+                      <p className="fw-bold">Company</p>
+                      <Link to="/about"><i className="fa-solid fa-circle-info me-2"></i>About</Link>
+                      <Link to="/products"><i className="fa-solid fa-box-open me-2"></i>Products</Link>
+                      <Link to="/pricing"><i className="fa-solid fa-tags me-2"></i>Pricing</Link>
+                      <Link to="/referral"><i className="fa-solid fa-user-friends me-2"></i>Referral programme</Link>
+                      <Link to="/careers"><i className="fa-solid fa-briefcase me-2"></i>Careers</Link>
+                      <Link to="/technest"><i className="fa-solid fa-laptop-code me-2"></i>TradeNest.tech</Link>
+                      <Link to="/press"><i className="fa-solid fa-newspaper me-2"></i>Press & media</Link>
+                      <Link to="/csr"><i className="fa-solid fa-hand-holding-heart me-2"></i>TradeNest cares (CSR)</Link>
                     </div>
-                </div>
-                <div className='col text-muted'>
-                    <p>Company</p>
-                    <Link href='' style={{textDecoration: "none"}}>About</Link>
-                    <br />
-                    <Link href=''style={{textDecoration: "none"}}>Products</Link>
-                    <br />
-                    <Link href=''style={{textDecoration: "none"}}>Pricing</Link>
-                    <br />
-                    <Link href=''style={{textDecoration: "none"}}>Referral programme</Link>
-                    <br />
-                    <Link href=''style={{textDecoration: "none"}}>Careers</Link>
-                    <br />
-                    <Link href=''style={{textDecoration: "none"}}>TradeNest.tech</Link>
-                    <br />
-                    <Link href=''style={{textDecoration: "none"}}>Press & media</Link>
-                    <br />
-                    <Link href=''style={{textDecoration: "none"}}>TradeNest cares (CSR)</Link>
-                    <br />
-                </div>
-                <div className='col'>
-                    <p>Support</p>
-                    <Link href='' style={{textDecoration: "none"}}>Contact</Link>
-                    <br />
-                    <Link href='' style={{textDecoration: "none"}}>Support portal</Link>
-                    <br />
-                    <Link href='' style={{textDecoration: "none"}}>Z-connect blog</Link>
-                    <br />
-                    <Link href='' style={{textDecoration: "none"}}>List of charges</Link>
-                    <br />
-                    <Link href='' style={{textDecoration: "none"}}>Downloads & resources</Link>
-                    <br />
-                </div>
-                <div className='col'>
-                    <p>Account</p>
-                    <Link href='' style={{textDecoration: "none"}}>Open an account</Link>
-                    <br />
-                    <Link href='' style={{textDecoration: "none"}}>Fund transfer</Link>
-                    <br />
-                    <Link href='' style={{textDecoration: "none"}}>60 day challenge</Link>
-                    <br />
-                </div>
+                    
+                    {/* Support Section */}
+                    <div className='col-6 col-md-3 mb-4 footer-links'>
+                      <p className="fw-bold">Support</p>
+                      <Link to="/contact"><i className="fa-solid fa-envelope me-2"></i>Contact</Link>
+                      <Link to="/support"><i className="fa-solid fa-headset me-2"></i>Support portal</Link>
+                      <Link to="/blog"><i className="fa-solid fa-blog me-2"></i>Z-connect blog</Link>
+                      <Link to="/charges"><i className="fa-solid fa-file-invoice-dollar me-2"></i>List of charges</Link>
+                      <Link to="/downloads"><i className="fa-solid fa-download me-2"></i>Downloads & resources</Link>
+                    </div>
+                    
+                    {/* Account Section */}
+                    <div className='col-6 col-md-3 mb-4 footer-links'>
+                      <p className="fw-bold">Account</p>
+                      <Link to="/open-account"><i className="fa-solid fa-user-plus me-2"></i>Open an account</Link>
+                      <Link to="/fund-transfer"><i className="fa-solid fa-money-check-dollar me-2"></i>Fund transfer</Link>
+                      <Link to="/challenge"><i className="fa-solid fa-trophy me-2"></i>60 day challenge</Link>
+                    </div>
+
             </div>
               <div className='mt-5 fs-6 text-muted'>
                 <p> Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Commodity Trading through Zerodha Commodities Pvt. Ltd. MCX: 46025; NSE-50001 – SEBI Registration no.: INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com, for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF </p>
--- frontend/src/Landing_page/Navbar.js
+++ frontend/src/Landing_page/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
     return (
-            <nav class="navbar navbar-expand-lg border-bottom" style={{backgroundColor:"#FFF"}}>
+            <nav class="navbar navbar-expand-lg border-bottom" style={{backgroundColor:"#080404"}}>
                 <div class="container p-2">
                     <Link class="navbar-brand" to="/"><img src='media/images/Logo.webp' style={{width: "30%"}} alt='Logo'/></Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
